88c31e0b7b7535c319f47f84483fe69daba6e3de
arunbanga <arunbanga810@gmail.com>
2022-09-16 20:13:25 +0530

    navlink add

--- src/Components/Header&Footer/Header.js
+++ src/Components/Header&Footer/Header.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header>
+        <NavLink to="/">
+          <p>Logo</p>
+        </NavLink>
+        <Navbar />
+      </header>
+    </>
+  );
 };
 
 export default Header;
